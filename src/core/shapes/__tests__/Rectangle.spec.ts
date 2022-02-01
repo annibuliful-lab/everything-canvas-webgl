b1dcd179bba17459f6e8cd486111eba254a93554
lagmanzaza <lagmanmc@gmail.com>
2022-02-01 08:11:35 +0700

    feat: rectangle

---
+++ src/core/shapes/__tests__/Rectangle.spec.ts
@@ -0,0 +1,108 @@
+import { ObjectPositioDimension } from "../../@types/Object";
+import { Rectangle } from "../Rectangle";
+
+const BASE_PARAM = {
+  x: 10,
+  y: 12,
+  width: 120,
+  height: 311,
+};
+
+const EXPECT_BOUNDING_BOX = {
+  topLeft: { x: BASE_PARAM.x, y: BASE_PARAM.y },
+  topRight: { x: 130, y: BASE_PARAM.y },
+  bottomLeft: { x: BASE_PARAM.x, y: 323 },
+  bottomRight: { x: 130, y: 323 },
+};
+
+function createRect(data?: ObjectPositioDimension) {
+  return new Rectangle(data ?? BASE_PARAM);
+}
+
+describe("Rectangle", () => {
+  const baseRect = createRect();
+  const baseRectObject = baseRect.toObject();
+  const baseBoundingBox = baseRectObject.boundingBox;
+
+  it("it should create Rectangle class", () => {
+    expect(baseRect).toBeDefined();
+  });
+  describe("bounding position", () => {
+    it("it should return correct top left position", () => {
+      expect(baseRect.topLeft).toEqual({ x: 10, y: 12 });
+    });
+
+    it("it should return correct top right position", () => {
+      expect(baseRect.topRight).toEqual({ x: 130, y: 12 });
+    });
+
+    it("it should return correct bottom left position", () => {
+      expect(baseRect.bottomLeft).toEqual({ x: 10, y: 323 });
+    });
+    it("it should return correct bottom right position", () => {
+      expect(baseRect.bottomRight).toEqual({ x: 130, y: 323 });
+    });
+  });
+
+  describe("toObject ", () => {
+    it("it should contain position object", () => {
+      expect(baseRectObject.x).toEqual(BASE_PARAM.x);
+      expect(baseRectObject.y).toEqual(BASE_PARAM.y);
+    });
+
+    it("it should contain dimension object", () => {
+      expect(baseRectObject.widht).toEqual(BASE_PARAM.width);
+      expect(baseRectObject.height).toEqual(BASE_PARAM.height);
+    });
+
+    it("it should contain top left object position", () => {
+      expect(baseBoundingBox.topLeft).toEqual(EXPECT_BOUNDING_BOX.topLeft);
+    });
+    it("it should contain top right object position", () => {
+      expect(baseBoundingBox.topRight).toEqual(EXPECT_BOUNDING_BOX.topRight);
+    });
+    it("it should contain bottom left object position", () => {
+      expect(baseBoundingBox.bottomLeft).toEqual(
+        EXPECT_BOUNDING_BOX.bottomLeft
+      );
+    });
+    it("it should contain bottom right object position", () => {
+      expect(baseBoundingBox.bottomRight).toEqual(
+        EXPECT_BOUNDING_BOX.bottomRight
+      );
+    });
+  });
+
+  describe("contains", () => {
+    it("it should be true when rectangle contains point(10,30)", () => {
+      expect(baseRect.contains({ x: 11, y: 30 })).toBeTruthy();
+    });
+
+    it("it should be true when rectangle contains point(60,60)", () => {
+      expect(baseRect.contains({ x: 60, y: 60 })).toBeTruthy();
+    });
+
+    it("it should be true when rectangle contains point(120,311)", () => {
+      expect(baseRect.contains({ y: 311, x: 120 })).toBeTruthy();
+    });
+
+    it("it should be true when rectangle contains point(90,130)", () => {
+      expect(baseRect.contains({ x: 90, y: 130 })).toBeTruthy();
+    });
+
+    it("it should be false when rectangle does not contain point(333,140)", () => {
+      expect(baseRect.contains({ x: 333, y: 140 })).toBeFalsy();
+    });
+
+    it("it should be false when rectangle does not contain point(5,5)", () => {
+      expect(baseRect.contains({ x: 5, y: 5 })).toBeFalsy();
+    });
+
+    it("it should be false when rectangle does not contain point(324,12", () => {
+      expect(baseRect.contains({ x: 324, y: 12 })).toBeFalsy();
+    });
+    it("ut should be false when rectangle does not contain point(324,355)", () => {
+      expect(baseRect.contains({ x: 324, y: 355 })).toBeFalsy();
+    });
+  });
+});
